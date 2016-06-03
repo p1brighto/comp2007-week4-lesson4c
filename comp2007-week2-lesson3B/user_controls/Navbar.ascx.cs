58cce0b6243483d8abe1aac11f24b1134924d9cd
p1brighto <p1brighto@gmail.com>
2016-06-03 10:31:23 -0400

    Add project files.

---
+++ comp2007-week2-lesson3B/user_controls/Navbar.ascx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/**
+ * @author: Brighto Paul
+ * @date: May 26, 2016
+ */
+
+namespace comp2007_week2_lesson3B
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+           protected void Page_Load(object sender, EventArgs e)
+            {
+                SetActivePage();
+            }
+
+            /**
+             * This method adds a css class of "active" to list items related
+             * to navigation links of each page 
+             * 
+             * @method SetActivePage
+             * @return {void}
+             */
+            private void SetActivePage()
+            {
+                switch (Page.Title)
+                {
+                    case "Home Page":
+                        home.Attributes.Add("class", "active");
+                        break;
+                    case "Contact":
+                        contact.Attributes.Add("class", "active");
+                        break;
+                }
+            }
+        }
+    }
